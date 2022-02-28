84449e64414f0e0eff0e782aaa151388b3e6cdbc
grennfp <50706093+grennfp@users.noreply.github.com>
2022-02-28 13:35:58 -0500

    Update 1_run_guppy_basecaller.sh
    alter memory and time of commented command for clarity

--- Analysis-pipeline/1_run_guppy_basecaller.sh
+++ Analysis-pipeline/1_run_guppy_basecaller.sh
@@ -5,4 +5,4 @@ OUT_PATH=$2
 
 guppy_basecaller -i ${FAST5_PATH} -s ${OUT_PATH} -c dna_r9.4.1_450bps_sup_prom.cfg -x cuda:all:100% -r --read_batch_size 250000 -q 250000
 
-#sbatch --partition=gpu --cpus-per-task=14 --mem=120g --gres=gpu:v100x:2,lscratch:200 --time=2-0 --wrap="bash 1_run_guppy_basecaller.sh /fast5_path /out_path"
+#sbatch --partition=gpu --cpus-per-task=14 --mem=10g --gres=gpu:v100x:2,lscratch:200 --time=4-0 --wrap="bash 1_run_guppy_basecaller.sh /fast5_path /out_path"
